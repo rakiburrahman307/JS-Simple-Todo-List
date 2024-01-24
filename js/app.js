8116d09af635dcfe1c8826da3eafff8fd0b5ca75
rakiburrahman307 <rakiburrahman307@gmail.com>
2024-01-24 12:55:55 +0600

    almost Done

--- js/app.js
+++ js/app.js
@@ -1,33 +1,42 @@
-const checkLocalStorage = () => {
-    const localStorageData = localStorage.getItem('todo-list');
-    return localStorageData ? JSON.parse(localStorageData) : [];
-};
-const saveLocalStorageData = checkLocalStorage();
 const updateDisplay = () => {
     const display = document.getElementById('display');
-    display.innerHTML = '';
-    saveLocalStorageData.forEach((item, idx) => {
-        const ul = document.createElement('ul');
-        ul.classList.add('display-ul');
-        ul.innerHTML = `<li class='listItem'>${item} <button onclick="deleteItem(${idx})" class="btn-2">Delete</button></li>`;
-        display.appendChild(ul);
+    const ul = document.createElement('ul');
+    ul.classList.add('display-ul');
+    const todoList = JSON.parse(localStorage.getItem('todo-list')) || [];
+    todoList.forEach((item, idx) => {
+        const li = document.createElement('li');
+        li.classList.add('listItem');
+        li.innerHTML = `${item} <button onclick="deleteItem('${idx}')" class="btn-2">Delete</button>`;
+        ul.appendChild(li);
     });
+
+    display.innerHTML = '';
+    display.appendChild(ul);
 };
 
-updateDisplay();
 document.getElementById('todo-btn').addEventListener('click', (e) => {
     e.preventDefault();
     try {
         const input = document.getElementById('todo-input').value;
-        const data = [...saveLocalStorageData, input];
-        localStorage.setItem('todo-list', JSON.stringify(data));
-        console.log('Data input successfully saved');
+        const saveData = JSON.parse(localStorage.getItem('todo-list') || '[]');
+        localStorage.setItem('todo-list', JSON.stringify([...saveData, input]));
         updateDisplay();
+        document.getElementById('todo-input').value = '';
     } catch (error) {
         console.error('Error saving data:', error);
     }
 });
 
 const deleteItem = (idx) => {
-    console.log(idx);
+    const confirmYes = confirm('Are you sure you want to delete');
+    if (confirmYes) {
+        const data = JSON.parse(localStorage.getItem('todo-list')) || [];
+        const storedData = [...data.slice(0, idx), ...data.slice(idx + 1)];
+        localStorage.setItem('todo-list', JSON.stringify(storedData));
+        updateDisplay();
+    } else {
+        alert("Data Is Not Deleted");
+    }
 };
+
+window.addEventListener('load', updateDisplay);
